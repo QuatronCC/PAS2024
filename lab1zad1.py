a77fb28b87f934a2eec0ca99e09127d2cfa98cf2
QuatronCC <113441867+QuatronCC@users.noreply.github.com>
2024-03-09 15:58:30 +0100

    Update lab1zad1.py

--- lab1zad1.py
+++ lab1zad1.py
@@ -1,13 +1,14 @@
-def copy_to_file(filename): 
-try: 
-  with open(filename, 'r') as input_file: 
-   contents = input_file.read() 
-   with open('lab1zad1.txt', 'w') as output_file: 
-    output_file.write(contents) 
-  print("File has been copied to lab1zad1.txt") 
-except FileNotFoundError: 
-   print("File does not exist") 
- 
-if __name__ == '__main__': 
-filename = input("Give a filename") 
+def copy_to_file(filename):
+    try:
+        with open(filename, 'r') as input_file:
+            contents = input_file.read()
+            with open('lab1zad1.txt', 'w') as output_file:
+                output_file.write(contents)
+        print("File has been copied to lab1zad1.txt")
+    except FileNotFoundError:
+        print("File does not exist")
+
+
+if __name__ == '__main__':
+    filename = input("Give a filename: ")
 copy_to_file(filename) 
