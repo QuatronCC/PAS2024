9bb63f557e17ae5a0522c0d201531ac96a2a0a49
QuatronCC <113441867+QuatronCC@users.noreply.github.com>
2024-03-09 15:55:31 +0100

    Update lab1zad1.py

--- lab1zad1.py
+++ lab1zad1.py
@@ -1,6 +1,6 @@
 def copy_to_file(filename): 
 try: 
-  with pen(filename, 'r') as input_file: 
+  with open(filename, 'r') as input_file: 
    contents = input_file.read() 
    with open('lab1zad1.txt', 'w') as output_file: 
     output_file.write(contents) 
