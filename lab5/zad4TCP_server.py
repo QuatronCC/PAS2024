8e3c3ef49c7cd858429e5d50ac24e272265b35aa
QuatronCC <113441867+QuatronCC@users.noreply.github.com>
2024-06-03 11:48:50 +0200

    Create zad4TCP_server.py

---
+++ lab5/zad4TCP_server.py
@@ -0,0 +1,23 @@
+import socket
+import time
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+def tcp_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(1)
+        print("TCP server is listening...")
+
+        conn, addr = server_socket.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+if __name__ == "__main__":
+    tcp_server()
